022f2a7489902b6986b002d81259888f72c54d92
Dthanit <43135317+Dthanit@users.noreply.github.com>
2019-01-20 02:16:49 +0700

    Update IntegerComparison.java

--- IntegerComparison.java
+++ IntegerComparison.java
@@ -3,32 +3,28 @@ import java.util.*;
 public class IntegerComparison
 {
     public static void main(String[]args)
-	{  
-      checkPositive(x);
-      checkNegative(x);
-    }
+      {  
+          checkPositive(x);
+          checkNegative(x);
+      }
     static boolean checkPoint(int x)
-    {
-     Scanner in = new Scanner(System.in); 
-     System.out.print("Enter Number => ");
-     int x = in.nextInt();
-     
-     if(x>0)
-     {return true;}
-     else
-     {return false;}
-     
-    }
+      {
+          Scanner in = new Scanner(System.in); 
+          System.out.print("Enter Number => ");
+          int x = in.nextInt();
+          if(x>0)
+          {return true;}
+            else
+            {return false;}
+      }
     static boolean checkNegative(int x)
-    {
-        Scanner in = new Scanner(System.in); 
-        System.out.print("Enter Number => ");
-        int x = in.nextInt();
-        
-      if(x<0)
-      {return true;}
-      else
-      {return false;}
-     
-    }
+      {
+          Scanner in = new Scanner(System.in); 
+          System.out.print("Enter Number => ");
+          int x = in.nextInt();
+          if(x<0)
+          {return true;}
+            else
+            {return false;}
+      }
 }
